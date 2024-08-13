0dbdd2deef5cd7b8e1664e9b6e05845b94bd161b
GoodBoyGilbert <96216895+GoodBoyGilbert@users.noreply.github.com>
2024-08-12 20:53:36 -0500

    Add files via upload

---
+++ number_guess.py
@@ -0,0 +1,10 @@
+import random
+
+guess = input("Please enter a number between 1 and 10: ")
+
+computer_guess = random.randint(0,10)
+
+print(" ")
+
+print(f"You guess: {guess}, the computer's guess:{computer_guess}")
+  
